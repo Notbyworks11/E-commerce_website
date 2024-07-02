79a535ba2ad7cc71053e1971fd278664f5c16b70
Andy Brenya <Kwadwoappiah479@gmail.com>
2024-07-02 23:21:32 +0100

    initial commit

---
+++ core/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render,HttpResponse
+from .models import Product
+# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render( request, "core/home.html",{'products':products})
