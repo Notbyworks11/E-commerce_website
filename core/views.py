553654f57de56fed4a218238d349a2064aba0f53
Andy Brenya <Kwadwoappiah479@gmail.com>
2024-07-10 16:58:40 +0100

    edited a few stuff login css

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render,HttpResponse
 from .models import Product
+from  django.contrib.auth import login, authenticate ,logout
+from django.contrib import messages
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    return render( request, "core/base.html",{'products':products})
+    return render( request, "products/products.html",{'products':products})
+
+def login_user(request):
+    return render(request,'Logs/Login.html',{})
+
+def logout_user(request):
+    pass
--- ecomprj/urls.py
+++ ecomprj/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from . import settings 
 from django.conf.urls.static  import static
-
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("core.urls"))
+    path("",include("core.urls")),
+    path("login/",views.login_user ,name='login'),
+    path("logout/",views.logout_user ,name='logout'),
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
