3a0761694ab12c77e7afec67fae93a58386216b2
Andy Brenya <Kwadwoappiah479@gmail.com>
2024-07-17 23:28:59 +0100

    jquery

--- cart/urls.py
+++ cart/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static  import static
-from cart import views
+from . import views
 
 urlpatterns = [
     path('', views.cart_summary, name='cart_summary' ),
